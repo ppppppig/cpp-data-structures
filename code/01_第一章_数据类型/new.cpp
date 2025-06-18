ad4e381264d418b77abca9a1be57475a73c75e68
MKING <2206229743@qq.com>
2025-06-18 21:02:13 +0800

    第一天学习内容提交

---
+++ code/01_第一章_数据类型/new.cpp
@@ -0,0 +1,11 @@
+//
+// Created by MKING on 2025/6/18.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("pause");
+    return 0;
+}
--- code/01_第一章_数据类型/字面常量.cpp
+++ code/01_第一章_数据类型/字面常量.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
-
+#define PI 3.14
 int main()
 {
-  return 0;
+    system("chcp 65001");
+    cout << "Hello World!" << endl;
+    cout << "你好，世界！" << endl;
+    cout << "圆周率是：" << PI << endl;
+    return 0;
 }
